daceb0d61ad0bec72732d8beadcdbb4c8337a679
semdegamer <82233210+semdegamer@users.noreply.github.com>
2025-03-25 18:05:36 +0100

    save

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-*/
-
 /* GET Profile Page */
 router.get('/profile', function(req, res) {
   res.render('profile', { title: 'Profile' });
