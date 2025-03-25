daa5d6b37fdedd5f17914da184c331baf28d0d92
semdegamer <82233210+semdegamer@users.noreply.github.com>
2025-03-25 22:40:10 +0100

    .

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,29 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var nav = ["messages", "courses"];
+
 /* GET Profile Page */
 router.get('/profile', function(req, res) {
-  res.render('profile', { title: 'Profile' });
+  res.render('user/profile', { nav: nav});
 });
 
 /* GET Courses Page */
 router.get('/courses', function(req, res) {
-  res.render('courses', { title: 'Courses' });
+  res.render('user/courses', { nav: nav});
 });
 
 /* GET Messages Page */
 router.get('/messages', function(req, res) {
-  res.render('messages', { title: 'Messages' });
+  res.render('user/messages', { nav: nav});
 });
 
 /* GET Login Page */
 router.get('/login', function(req, res) {
-  res.render('login', { title: 'Login' });
+  res.render('auth/login', { title: 'Login' });
 });
 
 /* GET Register Page */
 router.get('/register', function(req, res) {
-  res.render('register', { title: 'Register' });
+  res.render('auth/register', { title: 'Register'});
 });
 
 module.exports = router;
