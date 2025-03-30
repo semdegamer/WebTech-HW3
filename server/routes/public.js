bb050cef628b9f75f27da25cadac446e60d68e7d
semdegamer <82233210+semdegamer@users.noreply.github.com>
2025-03-30 14:27:10 +0200

    removed junk

--- server/routes/public.js
+++ server/routes/public.js
@@ -7,28 +7,9 @@ router.get('/', (req, res, next) => false /* check for logged in, something like
   res.render('user/home');
 });
 
-let storedRes = 0;
-
 // default homepage
 router.get('/', function(req, res) {
   res.render('public/home');
-});
-
-router.get('/events', function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    'Access-Control-Allow-Origin': '*'
-  });
-  res.write(`data: ${JSON.stringify({name: "test", data: "dtataatatatat"})}\n\n`);
-  return;
-
-  setInterval(() => {
-    console.log("event send");
-    res.write(`data: ${JSON.stringify({name: "test", data: "dtataatatatat"})}\n\n`);
-    //storedRes.write(JSON.stringify({name: "test", data: "dtataatatatat"}));
-    //storedRes.flush();
-  }, 2000);
 });
 
 module.exports = router;
