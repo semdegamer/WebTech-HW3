74fd7fb7f94104ed931241c9a8f2c60b2e40d2c6
semdegamer <82233210+semdegamer@users.noreply.github.com>
2025-03-31 17:07:28 +0200

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -57,14 +57,13 @@ function testCase(req, res, next) {
     loggedIn: true,
     Id: parseInt(req.query.name[3])
   }
+  req.session = {
+    loggedIn: true,
+    user: { studentId: parseInt(req.query.name[3]) }
+  }
   const addUser = (fname, lname, email, password) => {
     return req.db.runSql("INSERT INTO Student(firstName, lastName, email, password) VALUES(?, ?, ?, ?);", [fname, lname, email, password]);
   };
-
-  req.session = {
-    loggedIn: true,
-    userId: parseInt(req.query.name[3])
-  }
 
   const execute = (db, sql) => {
     return new Promise((resolve, reject) => {
@@ -94,9 +93,47 @@ function testCase(req, res, next) {
         res(result);
     });
   });
+  var nameList = [
+    'Time','Past','Future','Dev',
+    'Fly','Flying','Soar','Soaring','Power','Falling',
+    'Fall','Jump','Cliff','Mountain','Rend','Red','Blue',
+    'Green','Yellow','Gold','Demon','Demonic','Panda','Cat',
+    'Kitty','Kitten','Zero','Memory','Trooper','XX','Bandit',
+    'Fear','Light','Glow','Tread','Deep','Deeper','Deepest',
+    'Mine','Your','Worst','Enemy','Hostile','Force','Video',
+    'Game','Donkey','Mule','Colt','Cult','Cultist','Magnum',
+    'Gun','Assault','Recon','Trap','Trapper','Redeem','Code',
+    'Script','Writer','Near','Close','Open','Cube','Circle',
+    'Geo','Genome','Germ','Spaz','Shot','Echo','Beta','Alpha',
+    'Gamma','Omega','Seal','Squid','Money','Cash','Lord','King',
+    'Duke','Rest','Fire','Flame','Morrow','Break','Breaker','Numb',
+    'Ice','Cold','Rotten','Sick','Sickly','Janitor','Camel','Rooster',
+    'Sand','Desert','Dessert','Hurdle','Racer','Eraser','Erase','Big',
+    'Small','Short','Tall','Sith','Bounty','Hunter','Cracked','Broken',
+    'Sad','Happy','Joy','Joyful','Crimson','Destiny','Deceit','Lies',
+    'Lie','Honest','Destined','Bloxxer','Hawk','Eagle','Hawker','Walker',
+    'Zombie','Sarge','Capt','Captain','Punch','One','Two','Uno','Slice',
+    'Slash','Melt','Melted','Melting','Fell','Wolf','Hound',
+    'Legacy','Sharp','Dead','Mew','Chuckle','Bubba','Bubble','Sandwich','Smasher','Extreme','Multi','Universe','Ultimate','Death','Ready','Monkey','Elevator','Wrench','Grease','Head','Theme','Grand','Cool','Kid','Boy','Girl','Vortex','Paradox'
+  ];
+
+  function generate() {
+    return nameList[Math.floor( Math.random() * nameList.length )];
+  };
 
   const act1 = () => {
-    next();
+    return req.db.getSql(`SELECT * FROM Student S WHERE S.studentId = ?;`, [req.user.Id]
+    ).then((row) => {
+      if (!row) {
+        var fn = generate();
+        var ln = generate();
+        var pr = req.db.runSql("INSERT INTO Student(studentId, firstName, lastName, email, password) VALUES(?, ?, ?, ?, ?);", [req.user.Id, fn, ln, fn + "@" + ln + ".com", "password"]
+        ).then(req.db.runSql("INSERT INTO Friend(friendshipId, studentId) VALUES(?, ?);", [1, req.user.Id])
+        ).then(req.db.runSql("INSERT INTO ChatParticipent(chatId, studentId) VALUES(?, ?);", [1, req.user.Id]));
+        return pr;
+      }
+    }
+    ).then(() => next());
   };
   
   if (!testDb){
@@ -104,15 +141,10 @@ function testCase(req, res, next) {
     
     // Initialize database and create necessary tables
     fs.readFile("private/dbdef.txt"
-    ).then(() => addUser("p1", "1", "e1", "1")
-    ).then(() => addUser("p2", "2", "e2", "2")
     ).then(() => req.db.runSql("INSERT INTO Friendship(date) VALUES(?);", [dayjs().format('YYYY/MM/DD')])
-    ).then(() => req.db.runSql("INSERT INTO Friend(friendshipId, studentId) VALUES(?, ?);", [1, 1])
-    ).then(() => req.db.runSql("INSERT INTO Friend(friendshipId, studentId) VALUES(?, ?);", [1, 2])
-    ).then(() => req.db.runSql("INSERT INTO Chat(creationDate, name) VALUES(strftime('%Y-%m-%d','now'), ?);", ["the chat"])
-    ).then(() => req.db.runSql("INSERT INTO ChatParticipent(chatId, studentId) VALUES(?, ?);", [1, 1])
-    ).then(() => req.db.runSql("INSERT INTO ChatParticipent(chatId, studentId) VALUES(?, ?);", [1, 2])
-    ).then(() => act1());
+    ).then(() => req.db.runSql("INSERT INTO Chat(creationDate, name) VALUES(strftime('%Y-%m-%d','now'), ?);", ["global chat"])
+    ).then(() => act1()
+    ).catch(err => console.log(err));
   }
   else
     act1();
